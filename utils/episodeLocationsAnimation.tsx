28497338ec5f979669a5a1eb3eb908caa3a8ee80
nahuoo <nahuel.sigismondi@gmail.com>
2022-07-06 06:36:03 -0300

    fix: animation config

--- utils/episodeLocationsAnimation.tsx
+++ utils/episodeLocationsAnimation.tsx
@@ -3,14 +3,18 @@ export const episodeLocationsAnimation = () => {
   let tl = gsap.timeline({
     defaults: {
       ease: 'none',
-      duration: 0.7,
+      duration: 1,
       stagger: 0.1,
     },
   })
-  tl.fromTo('.episodeSection', { opacity: 0 }, { opacity: 1 })
+  tl.fromTo(
+    '.episodeSection',
+    { y: '-10%', opacity: 0 },
+    { y: '0%', opacity: 1 }
+  )
   tl.fromTo(
     '.episodeCards',
-    { y: '-20%', ease: 'bounce.out', opacity: 0 },
+    { y: '-10%', ease: 'bounce.out', opacity: 0 },
     { y: '0%', ease: 'bounce.out', opacity: 1 },
     '<'
   )
